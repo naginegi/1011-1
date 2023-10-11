c40fcda352c51c732eb62421c4c9831238423c8a
naginegi <2000vsteven@gmail.com>
2023-10-11 12:06:26 +0800

    .

--- 1011-1/test.js
+++ 1011-1/test.js
@@ -1,12 +1,16 @@
-export const a =2 ; //匯出
-export const b ="123" ; //匯出
-export const c =4 ; //匯出
-export const d ="789" ; //匯出
+//具名匯出 可以有很多個
+export const a =2 ; 
+export const b ="123" ; 
+export const c =4 ; 
+export const d ="789" ; 
 
-//具名匯出 只能有一個
+//預設匯出 只能有一個
 //const arr = [1,2,3,4,5,6]
-const obj ={
-    name:"aaa",
-    number:777
+// const obj ={
+//     name:"aaa",
+//     number:777
+// }
+function fu(){
+    console.log("test.js")
 }
-export default obj;
+export default fu;
--- 1011-1/test2.js
+++ 1011-1/test2.js
@@ -3,11 +3,12 @@ import {a} from "./test.js";
 import {b} from "./test.js";  
 import {c,d} from "./test.js";  
 
-//具名匯入
+//預設匯入
 import x from "./test.js";
 
 console.log("a="+a);
 console.log("b="+b);
 console.log("c="+c);
 console.log("d="+d);
+x();
 console.log(x);
