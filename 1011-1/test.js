4d236759df3e0af03900611f9f9be477f26fd4e3
naginegi <2000vsteven@gmail.com>
2023-10-11 11:48:02 +0800

    .

--- 1011-1/test.js
+++ 1011-1/test.js
@@ -3,6 +3,10 @@ export const b ="123" ; //匯出
 export const c =4 ; //匯出
 export const d ="789" ; //匯出
 
-
 //具名匯出 只能有一個
-export default "000";
+//const arr = [1,2,3,4,5,6]
+const obj ={
+    name:"aaa",
+    number:777
+}
+export default obj;
--- 1011-1/test2.js
+++ 1011-1/test2.js
@@ -1,6 +1,7 @@
-import {a} from "./test.js";  //匯入
-import {b} from "./test.js";  //匯入
-import {c,d} from "./test.js";  //可以一次匯入多個
+//可以一次匯入多個
+import {a} from "./test.js"; 
+import {b} from "./test.js";  
+import {c,d} from "./test.js";  
 
 //具名匯入
 import x from "./test.js";
